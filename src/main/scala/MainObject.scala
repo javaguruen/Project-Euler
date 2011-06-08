f56dffb5735b891e0679da270de7a0b10999a2c7
Bjorn Hamre <javaguruen@gmail.com>
2011-06-08 21:46:52 +0200

    Endring gjort for oppgave 49

--- src/main/scala/MainObject.scala
+++ src/main/scala/MainObject.scala
@@ -5,10 +5,11 @@ object MainObject{
   def main(args : Array[String])  {
     val start = new Date()
     println( "Starter kjøring: " + start)
-    val euler = new Euler48
-    euler.run
+    val euler = new Euler49
+    val resultat = euler.run
     val ferdig = new Date()
     println( "Ferdig: " + ferdig)
     println("Tid medgått: " + (ferdig.getTime-start.getTime())/1000.0 + " sek.")
+    println("Resultat = " + resultat)
   }
 }
