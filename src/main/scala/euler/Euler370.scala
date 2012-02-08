49d2c5c7e8e5a296267d48dbb7475a48019f620d
Bjorn Hamre <javaguruen@gmail.com>
2012-02-09 00:27:09 +0100

    Kjører eksempelet til 370 på 15 sek, men mangler noen treff

--- src/main/scala/euler/Euler370.scala
+++ src/main/scala/euler/Euler370.scala
@@ -1,37 +1,47 @@
 package euler
 
 import org.slf4j.LoggerFactory
-
+import scala.math._
 
 class Euler370 {
 
   val logger = LoggerFactory.getLogger("Euler370")
 
-  def isGeometricTriangle(a: Long, b: Long, c: Long)= (b*b==a*c)
+  def isGeometricTriangle(a: Long, b: Long, c: Long) = (b * b == a * c)
+
+  val maksOmkrets = (scala.math.pow(10, 6)).toLong
+
+
+  def isHeltall(tall: Double): Boolean = {
+    abs(tall - floor(tall + 0.5)) < 0.00001
+  }
+
+  def litenNokOmkrets(a: Long, b: Long, c: Long): Boolean = (a + b + c) <= maksOmkrets
 
   def run(): Long = {
-    val maksOmkrets = (scala.math.pow(10, 6)).toLong
+    val rangeEnkel = List.range(1, sqrt(maksOmkrets).toInt + 10)
+    val rangeSqr = rangeEnkel.map(a => a * a)
+    println(rangeSqr)
+
     //val maksOmkrets = (2.5 * scala.math.pow(10, 13)).toLong
     var a = 1
-    var b = 1
-    var c = 1
     var geometricTriangles = 0
+    println("maksOmkrets/3=" + maksOmkrets/3)
 
-    for (a <- 1 to (maksOmkrets/3).toInt ) {
-      logger.info("*** A=" + a)
-      for (b <- a to maksOmkrets.toInt ) {
-        if( b % 10000 == 0) logger.info("****** B=" + b)
-        for (c <- b to b*b ) {
-          if( a+b+c <= maksOmkrets){
-            if( isGeometricTriangle(a, b, c) ){
-              geometricTriangles += 1
-              logger.info("Fant geometrisk triangel nr " + geometricTriangles + " for a=" + a + ", b=" + b + ", c="+c)
-            }
+    for (a <- 1 to (maksOmkrets / 3).toInt + 1) {
+      //logger.info("*** A=" + a)
+      rangeSqr.foreach(rangeElem => {
+        val c: Long = rangeElem * a
+        if (c > 0) {
+          val b = scala.math.sqrt(a * c).toInt
+          if (litenNokOmkrets(a, b, c)) {
+            geometricTriangles += 1
+            logger.info("Fant geometrisk triangel nr " + geometricTriangles + " for a=" + a + ", b=" + b + ", c=" + c)
           }
         }
       }
+      )
     }
-
     geometricTriangles
   }
 }
